4ab9b48f71986ac224c2052ff40cfe85f0bd5f79
sadxdev <sadikxdev@gmail.com>
2025-09-10 21:22:03 +0530

    Initial setup

---
+++ src/main/java/com/bashverse/motogalaxybackend/MotogalaxyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bashverse.motogalaxybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotogalaxyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotogalaxyBackendApplication.class, args);
+    }
+
+}
