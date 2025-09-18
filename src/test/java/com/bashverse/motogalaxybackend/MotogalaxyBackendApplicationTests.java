fc5b407e40958cd2eebea861fa4f2aebede36c61
sadxdev <sadikxdev@gmail.com>
2025-09-18 21:14:36 +0530

    other files

---
+++ src/test/java/com/bashverse/motogalaxybackend/MotogalaxyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bashverse.motogalaxybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotogalaxyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
