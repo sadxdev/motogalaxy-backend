91efa065f6a116eac865d8b17edd203c6046e6a1
sadxdev <sadikxdev@gmail.com>
2025-09-18 21:12:01 +0530

    Add domain enums for account status and user roles

---
+++ src/main/java/com/bashverse/motogalaxybackend/domain/AccountStatus.java
@@ -0,0 +1,10 @@
+package com.bashverse.motogalaxybackend.domain;
+
+public enum AccountStatus {
+    PENDING_VERIFICATION,
+    ACTIVE,
+    SUSPENDED,
+    DEACTIVATED,
+    BANNED,
+    CLOSED
+}
---
+++ src/main/java/com/bashverse/motogalaxybackend/domain/USER_ROLE.java
@@ -0,0 +1,8 @@
+package com.bashverse.motogalaxybackend.domain;
+
+public enum USER_ROLE {
+
+    ROLE_CUSTOMER,
+    ROLE_SELLER,
+    ROLE_ADMIN
+}
