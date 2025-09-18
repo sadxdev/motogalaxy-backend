2b1ef2623e970533a430d585507ea5bd25eafd40
sadxdev <sadikxdev@gmail.com>
2025-09-18 21:12:12 +0530

    Add authentication REST controller

---
+++ src/main/java/com/bashverse/motogalaxybackend/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.bashverse.motogalaxybackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+
+}
